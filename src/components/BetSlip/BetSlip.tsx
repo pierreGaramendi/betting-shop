2f8c920bba41670219390507f5958350b6a0b962
Pierre Garamendi Marquez <pirre.garamendi@gmail.com>
2022-04-09 17:38:04 -0500

    chore(BetSlip):button for selections were changed for a custom mini card

--- src/components/BetSlip/BetSlip.tsx
+++ src/components/BetSlip/BetSlip.tsx
@@ -2,16 +2,19 @@ import styles from './BetSlip.module.css';
 import Button from '@mui/material/Button';
 import { Interfaces } from '../../interfaces/Interfaces';
 
-function BetSlip(props: Interfaces.Market) {    
+function BetSlip(props: Interfaces.Market) {
     return (
         <div className={styles.BetSlip}>
             <div className={styles.cardbody}>
                 <div className={styles.cardTitle}>{props.name}</div>
             </div>
             <div className={styles.cardfooter}>
-                {props.selections.map((item: Interfaces.Selection) => {                    
-                    return (                        
-                            <Button variant="contained">{item.name}</Button>                                                
+                {props.selections.map((item: Interfaces.Selection) => {
+                    return (
+                            <div className={styles.selectionCard}>
+                                <div>{item.name}</div>
+                                <div>{item.price}</div>
+                            </div>                
                     );
                 })}
             </div>
